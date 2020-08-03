7d6a587239ff33116b94feaad58f9ea39a077702
Balazs Nyirfa <balazsnyirfa@gmail.com>
2020-08-03 17:40:22 +0200

    add new nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,21 @@
 <template>
   <v-app>
-    <v-navigation-drawer
-      v-model="drawer"
-      app
-      clipped>
-      <v-list dense>
-        <v-list-item link :to="{path:'/'}">
-          <v-list-item-content>
-            <v-icon>fa-home</v-icon>
-            <v-list-item-title>Home</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-        <v-list-item link :to="{path:'/serieslist'}">
-          <v-list-item-content>
-            <v-icon>fa-list</v-icon>
-            <v-list-item-title>List</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-        <v-list-item link :to="{path:'/upload'}">
-          <v-list-item-content>
-            <v-icon>fa-upload</v-icon>
-            <v-list-item-title>Upload</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-app-bar
-      app
-      clipped-left>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-toolbar-title>Application</v-toolbar-title>
-    </v-app-bar>
+    <Nav />
     <router-view/>
   </v-app>
 </template>
 
 
 <script>
+import Nav from "./components/Nav.vue"
+
   export default {
+    components: {
+      Nav
+    },
     props: {
       source: String,
     },
-    data: () => ({
-      drawer: false,
-    }),
     created () {
       this.$vuetify.theme.dark = true
     },
@@ -57,7 +29,6 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #cbe6fb;
-  background-color: rgb(71, 71, 71);
+  background-color: #181a1b;
 }
 </style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-bottom-navigation
+    :value="activeBtn"
+    grow
+    color="red"
+  >
+    <v-btn :to="{path:'/'}">
+      <span>Home</span>
+      <v-icon>fa-home</v-icon>
+    </v-btn>
+
+    <v-btn :to="{path:'/serieslist'}">
+      <span>Library</span>
+      <v-icon>fa-list</v-icon>
+    </v-btn>
+
+    <v-btn :to="{path:'/upload'}">
+      <span>Upload</span>
+      <v-icon>fa-upload</v-icon>
+    </v-btn>
+  </v-bottom-navigation>
+</template>
+
+<script>
+  export default {
+    name: "Nav"
+  }
+</script>
