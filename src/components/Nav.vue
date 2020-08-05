2fa425a678166e85957d8e4cc9dda5b1a9c8eeb4
Balazs Nyirfa <balazsnyirfa@gmail.com>
2020-08-05 13:32:50 +0200

    add server storage status (not live yet)

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -12,11 +12,19 @@
       <span>Library</span>
       <v-icon>fa-list</v-icon>
     </v-btn>
-
     <v-btn :to="{path:'/upload'}">
       <span>Upload</span>
       <v-icon>fa-upload</v-icon>
     </v-btn>
+    <v-tooltip bottom color="red">
+      <template v-slot:activator="{ on, attrs }">
+        <div id="storageBox" v-bind="attrs" v-on="on">
+          <v-icon>fa-hdd</v-icon><br>
+          <span><strong>0/1200GB</strong></span>
+        </div>
+      </template>
+      <span>This feature is not live yet!</span>
+    </v-tooltip>
   </v-bottom-navigation>
 </template>
 
@@ -25,3 +33,12 @@
     name: "Nav"
   }
 </script>
+
+<style lang="scss" scoped>
+#storageBox {
+  padding: 0.5rem;
+  font-size: 0.95rem;
+  display: block;
+  margin-left:auto; margin-right:0;
+}
+</style>
